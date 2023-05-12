8cdff1d92c7d5b759b4e59f0ef9e212bc00722ea
rafu01 <nrafsan13@gmail.com>
2023-05-12 20:03:47 +0600

    Initialize Spring Boot application

---
+++ src/main/java/com/sure/surepreview/SurePreviewApplication.java
@@ -0,0 +1,13 @@
+package com.sure.surepreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurePreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurePreviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sure/surepreview/SurePreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sure.surepreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurePreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
