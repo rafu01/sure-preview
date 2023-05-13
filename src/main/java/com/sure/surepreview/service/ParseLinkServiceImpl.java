b0499fef9d18526d3207cf45a7f88b8ec2837af0
rafu01 <nrafsan13@gmail.com>
2023-05-13 21:25:11 +0600

    Handle Tags that are not Present

--- src/main/java/com/sure/surepreview/service/ParseLinkServiceImpl.java
+++ src/main/java/com/sure/surepreview/service/ParseLinkServiceImpl.java
@@ -1,19 +1,21 @@
 package com.sure.surepreview.service;
 
-
 import com.sure.surepreview.dto.PreviewDto;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ParseLinkServiceImpl implements ParseLinkService{
 
     @Override
     @SneakyThrows
     public PreviewDto parseLink(String link){
         var document = Jsoup.connect(link)
+                .userAgent("Mozilla")
                 .followRedirects(true).get();
         return PreviewDto.builder()
                 .title(getTitle(document))
@@ -23,14 +25,59 @@ public class ParseLinkServiceImpl implements ParseLinkService{
     }
 
     private String getTitle(Document document){
-        return document.title();
+        try {
+            return document.title();
+        }
+        catch (NullPointerException exception){
+            return getTitleFromOgTag(document);
+        }
     }
 
-    private String  getImage(Document document){
+    private String getTitleFromOgTag(Document document){
+        try {
+            return document.select("meta[property=og:title]").first().attr("content");
+        }
+        catch (NullPointerException exception){
+            log.error("Could Not Get Parse Title From: {}", document.location());
+            return "";
+        }
+    }
+
+    private String getDescription(Document document){
+        try {
+            return getDescriptionFromMeta(document);
+        }
+        catch (NullPointerException exception){
+            return getDescriptionFromOgTag(document);
+        }
+    }
+
+    private String  getDescriptionFromOgTag(Document document){
+        try {
+            return document.select("meta[property=og:description]").first().attr("content");
+        }
+        catch (NullPointerException exception){
+            log.error("Could Not Get Parse Description From: {}", document.location());
+            return "";
+        }
+    }
+
+    private String getDescriptionFromMeta(Document document){
+        return document.select("meta[name=description]").first().attr("content");
+    }
+
+    private String getImage(Document document){
+        try {
+            return getImageFromOgTag(document);
+        }
+        catch (NullPointerException exception){
+            log.error("Could Not Get Parse Image From: {}", document.location());
+            return "";
+        }
+    }
+
+    private String  getImageFromOgTag(Document document){
         return document.select("meta[property=og:image]").first().attr("content");
     }
 
-    private String  getDescription(Document document){
-        return document.select("meta[property=og:description]").first().attr("content");
-    }
 }
